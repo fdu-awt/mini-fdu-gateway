26a1c4357ea6df9393ca533c37c4c3226bd358c5
zmark <351479188@qq.com>
2024-06-11 14:47:21 +0800

    init

---
+++ src/main/java/org/minifdu/minifdugateway/MiniFduGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.minifdu.minifdugateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFduGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniFduGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/minifdu/minifdugateway/MiniFduGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minifdu.minifdugateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFduGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
